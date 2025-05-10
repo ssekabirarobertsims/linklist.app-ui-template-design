ce20e5e82a04a344d3c74438bd4831301f156a5e
ssekabira robert sims <ssekabirarobert037@gmail.com>
2025-05-10 15:30:45 +0300

    ********

--- src/pages/Offline.Page.tsx
+++ src/pages/Offline.Page.tsx
@@ -3,6 +3,7 @@ import "../stylesheets/Offline.Page.Stylesheet.css";
 import CookiesSiteMessageComponent from "../components/Cookies.Site.Message.Component";
 import NotificationsSideBarComponent from "../components/Notifications.Side.Bar.Component";
 import SiteDeveloperNavigationComponent from "../components/Developer.Navigation.Div.Component";
+import SecondaryNavigationBarComponent from "../components/Secondary.Navigation.Bar.Component";
 
 const OfflinePageElementsComponent: React.FunctionComponent = () => {
      React.useEffect(() => {
@@ -13,6 +14,7 @@ const OfflinePageElementsComponent: React.FunctionComponent = () => {
         <CookiesSiteMessageComponent />
         <SiteDeveloperNavigationComponent />
         <NotificationsSideBarComponent />
+        <SecondaryNavigationBarComponent />
          <section className={String("offline-page-elements-component").toLocaleLowerCase()}>
             <div className={String("offline-page-elements-component-content-wrapper").toLocaleLowerCase()}>
                 <h1>Network Error!</h1>
